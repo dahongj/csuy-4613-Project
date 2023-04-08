4384548a320f2eb9d5bdb0561745f597ee7d88c8
dahongj <dj1390@nyu.edu>
2023-04-08 23:21:08 +0000

    page

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ import streamlit as st
 from transformers import pipeline
 
 
-st.title("Sentiment Analysis")
-userText = st.text_input('User Input', 'Hope you are having a great day!')
+def rendPage():
+    st.title("Sentiment Analysis")
+    userText = st.text_input('User Input', "Hope you are having a great day!")
+    st.text("")
+    type = st.selectbox(
+        'Choose your model',
+        ()
+    )
+
+    if st.button('Calculate'):
+        if(userText!="" and type != None):
+            st.text
+
+rendPage
