a6ed43c891108f8b7baceb424ccf20fb3ba429bf
dahongj <33811542+dahongj@users.noreply.github.com>
2023-04-09 10:46:08 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import streamlit as st
+from transformers import pipeline
+
+def bertweet(data):
+    specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+    print(specific_model(data))
+
+def getSent(data, model):
+    if(model == 'Bertweet'):
+        bertweet(data)
+
+
+def rendPage():
+    st.title("Sentiment Analysis")
+    userText = st.text_input('User Input', "Hope you are having a great day!")
+    st.text("")
+    type = st.selectbox(
+        'Choose your model',
+        ('Bertweet','Roberta'))
+    st.text("")
+
+    if st.button('Calculate'):
+        if(userText!="" and type != None):
+            st.text
+            getSent(userText,type)
+
+rendPage()
